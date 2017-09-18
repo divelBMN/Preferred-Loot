b1ecac5dbbf41f1d68952023191172198e6b8298
Maxim Bezdenezhnykh <esgiliot@gmail.com>
2017-09-18 22:43:13 +0500

    first commit

---
+++ src/neural_network/Vector.java
@@ -0,0 +1,80 @@
+/**
+ * @author Maxim Bezdenezhnykh
+ * @email esgiliot@gmail.com
+ */
+
+package neural_network;
+
+/**
+ * Class Vector use for work with input/output signals and weights of neurons.
+ */
+public class Vector {
+    
+    private float [] values;
+    
+    
+    
+    /**
+     * Constructor.
+     * Make Vector with known values.
+     * @param values 
+     */
+    public Vector(float [] values) {
+        this.values = values;
+    }    
+    
+    /**
+     * Constructor.
+     * Make vector with values is zero.
+     * @param size 
+     */
+    public Vector(int size) {
+        this.values = this.zeroVector(size);
+    }
+    
+    
+    
+    /**
+     * Interface for getting value by index.
+     * @param index
+     * @return 
+     */
+    public float value(int index) throws IndexOutOfBoundsException {
+        if (index < this.values.length) {
+            return this.values[index];
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    
+    /**
+     * Interface for setting value by index.
+     * @param value
+     * @param index
+     * @throws IndexOutOfBoundsException 
+     */
+    public void setValue(float value, int index) throws IndexOutOfBoundsException {
+        if (index < this.values.length) {
+            this.values[index] = value;
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    
+            
+    
+    /**
+     * Creating array of float with values is zero
+     * @param size of array
+     * @return 
+     */
+    private float [] zeroVector(int size) {
+        float[] result = new float[size];
+        
+        for (int i = 0; i < size; i++) {
+            result[i] = 0;
+        }
+        
+        return result;
+    }
+}
---
+++ test/neural_network_tests/VectorTests.java
@@ -0,0 +1,62 @@
+/**
+ * @author Maxim Bezdenezhnykh
+ * @email esgiliot@gmail.com
+ */
+
+package neural_network_tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import neural_network.Vector;
+
+public class VectorTests {
+    
+    @Test
+    public void test_vector_0_1f_0_value_by_index_1_return_1f() {
+        Vector vector = new Vector(new float[] {0, 1f, 0});
+        assertEquals(vector.value(1), 1f, 0);
+    }
+    
+    @Test
+    public void test_vector_0_1f_0_value_by_index_3_return_IndexOutOfBoundsException() {
+        Vector vector = new Vector(new float[] {0, 1f, 0});
+        
+        boolean thrown = false;
+        try {
+            float value = vector.value(3);
+        } catch (IndexOutOfBoundsException e) {
+            thrown = true;
+        }
+        
+        assertTrue(thrown);
+    }
+    
+    @Test
+    public void test_vector_0_0_0_0_setValue_1f_index_1_return_0_1f_0_0() {
+        int vectorSize = 4;
+        Vector vector = new Vector(vectorSize);
+        vector.setValue(1f, 1);
+        
+        assertEquals(vector.value(0), 0, 0);
+        assertEquals(vector.value(1), 1f, 0);
+        assertEquals(vector.value(2), 0, 0);
+        assertEquals(vector.value(3), 0, 0);
+    }
+    
+    @Test
+    public void test_vector_0_0_0_setValue_by_index_3_return_IndexOutOfBoundsException() {
+        int vectorSize = 3;
+        Vector vector = new Vector(vectorSize);
+        
+        boolean thrown = false;
+        try {
+            float value = vector.value(3);
+        } catch (IndexOutOfBoundsException e) {
+            thrown = true;
+        }
+        
+        assertTrue(thrown);
+    }
+    
+}
